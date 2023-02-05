3b5e4eb1ad431fbb78ba4257f96d069d6f6122fa
Rob Speiran <rspeiran@outlook.com>
2023-02-04 20:38:55 -0500

    Lights on and Off

--- SDHSTestBot/src/main/java/frc/robot/commands/LightOff.java
+++ SDHSTestBot/src/main/java/frc/robot/commands/LightOff.java
@@ -31,6 +31,7 @@ public class LightOff extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
+        m_lights.TurnTheLightOff();
     }
 
     // Called once the command ends or is interrupted.
@@ -41,7 +42,7 @@ public class LightOff extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return true;
     }
 
     @Override
